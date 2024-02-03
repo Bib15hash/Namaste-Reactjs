96dcc4198e87afd0e9c339ccbabcf1652a729571
Bibhash Sarkar <bibhashsarkar15042001@gmail.com>
2024-02-04 03:40:29 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,33 @@
+import RestaurantCard from "./RestaurantCard.js";
+import resList from "../utils/mockData.js";
+import { useState } from "react";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/utils/mockData.js
@@ -0,0 +1,670 @@
+const resList = [
+  {
+    info: {
+      id: "640980",
+      name: "Turquoise Kitchen",
+      cloudinaryImageId: "ab64fdd35f9498b56f85228f2cda12c6",
+      locality: "Janapath Road",
+      areaName: "Bharalumukh",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian", "Continental", "Burgers", "Chinese"],
+      avgRating: 3.8,
+      parentId: "386675",
+      avgRatingString: "3.8",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 51,
+        lastMileTravel: 13.4,
+        serviceability: "SERVICEABLE",
+        slaString: "50-55 mins",
+        lastMileTravelString: "13.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-01-14 02:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "15% OFF",
+        subHeader: "ABOVE ₹700",
+        discountTag: "FLAT DEAL",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/turquoise-kitchen-janapath-road-bharalumukh-guwahati-640980",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "456371",
+      name: "KFC",
+      cloudinaryImageId: "f01666ac73626461d7455d9c24005cd4",
+      locality: "Lokpriya Gopinath Bordoloi International Airport",
+      areaName: "Borjhar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 4.4,
+      parentId: "547",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 4.5,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "25-30 mins",
+        lastMileTravelString: "4.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 10 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kfc-lokpriya-gopinath-bordoloi-international-airport-borjhar-guwahati-456371",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "569828",
+      name: "Flavours Of Darjeeling",
+      cloudinaryImageId: "exxe5mnnei7p5lefsmtj",
+      locality: "Goalpara Guwahati Road",
+      areaName: "Borjhar",
+      costForTwo: "₹500 for two",
+      cuisines: ["Chinese", "Burgers", "Fast Food"],
+      avgRating: 4.2,
+      parentId: "340487",
+      avgRatingString: "4.2",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 30,
+        lastMileTravel: 4,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "25-30 mins",
+        lastMileTravelString: "4.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 11 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/flavours-of-darjeeling-goalpara-road-borjhar-guwahati-569828",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "674839",
+      name: "Mast Biryani",
+      cloudinaryImageId: "288abbc58babf7b063b256e729b045b9",
+      locality: "AT Road",
+      areaName: "Azara",
+      costForTwo: "₹450 for two",
+      cuisines: [
+        "Biryani",
+        "Mughlai",
+        "North Indian",
+        "Indian",
+        "Tandoor",
+        "Kebabs",
+      ],
+      avgRating: 4.4,
+      parentId: "8229",
+      avgRatingString: "4.4",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 4.6,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "30-35 mins",
+        lastMileTravelString: "4.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 11 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹179",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mast-biryani-at-road-azara-guwahati-674839",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "696099",
+      name: "Baskin Robbins - Ice Cream Desserts",
+      cloudinaryImageId: "85ccae4e3576f9330af102c46ca85395",
+      locality: "Khanamukh",
+      areaName: "Dharapur",
+      costForTwo: "₹250 for two",
+      cuisines: ["Desserts", "Ice Cream"],
+      veg: true,
+      parentId: "5588",
+      avgRatingString: "--",
+      sla: {
+        deliveryTime: 31,
+        lastMileTravel: 4.6,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "30-35 mins",
+        lastMileTravelString: "4.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Temporarily closed for delivery",
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-khanamukh-dharapur-guwahati-696099",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "714218",
+      name: "Maa Ki Rasoi",
+      cloudinaryImageId: "4b20518cd1a1ce3da5b714c4e7fcb3fc",
+      locality: "Baruah Store",
+      areaName: "Dharapur",
+      costForTwo: "₹200 for two",
+      cuisines: ["North Indian", "Chinese", "Rolls & Wraps"],
+      avgRating: 2.7,
+      parentId: "4107",
+      avgRatingString: "2.7",
+      totalRatingsString: "9",
+      sla: {
+        deliveryTime: 35,
+        lastMileTravel: 4.2,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "35-40 mins",
+        lastMileTravelString: "4.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 10 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/maa-ki-rasoi-baruah-store-dharapur-guwahati-714218",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "657475",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "NCS Square Mall",
+      areaName: "Adabari",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 68,
+        lastMileTravel: 9.7,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "65-70 mins",
+        lastMileTravelString: "9.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 10 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-ncs-square-mall-adabari-guwahati-657475",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "408176",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Assam Trunk Road",
+      areaName: "Maligaon",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.4,
+      parentId: "721",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 73,
+        lastMileTravel: 9.7,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "70-75 mins",
+        lastMileTravelString: "9.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 11 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹179",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-assam-trunk-road-maligaon-guwahati-408176",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "617558",
+      name: "Starbucks Coffee",
+      cloudinaryImageId: "258fe8a3577877fbfe064095ed1d9dc3",
+      locality: "New Airport Road",
+      areaName: "Borjhar",
+      costForTwo: "₹400 for two",
+      cuisines: [
+        "Beverages",
+        "Cafe",
+        "Snacks",
+        "Desserts",
+        "Bakery",
+        "Ice Cream",
+      ],
+      avgRating: 4.2,
+      parentId: "195515",
+      avgRatingString: "4.2",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 45,
+        lastMileTravel: 4.3,
+        serviceability: "SERVICEABLE_WITH_BANNER",
+        slaString: "45-50 mins",
+        lastMileTravelString: "4.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextOpenTimeMessage: "Opens next at 10 am, today",
+      },
+      badges: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-946f1d7f-9737-4cea-8658-ca62dd5f0cfe",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/starbucks-coffee-new-airport-road-borjhar-guwahati-617558",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+export default resList;
